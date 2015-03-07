33feaffc66074e232bc546bbcd5d1e0ec7a797b6
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2015-03-07 17:15:06 +0330

    Add Middle Square Method to random class

--- src/com/newvertex/randgen/RandomGenerator.java
+++ src/com/newvertex/randgen/RandomGenerator.java
@@ -1,5 +1,6 @@
 package com.newvertex.randgen;
 
+import com.newvertex.randgen.util.Random;
 import com.newvertex.randgen.view.RootLayoutController;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -30,10 +31,10 @@ public class RandomGenerator extends Application {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("view/RootLayout.fxml"));
             AnchorPane rootLayout = loader.load();
-            
+
             RootLayoutController controller = loader.getController();
             controller.setApp(this);
-            
+
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
             primaryStage.setTitle("Random Generator");
@@ -54,6 +55,38 @@ public class RandomGenerator extends Application {
     }
 
     public void generateRandom(int start, int end, int number, boolean isMiddleSquare) {
-        
+        Random rnd = new Random(start, end);
+        int seed = getInitSeed();
+
+        if (isMiddleSquare) {
+            itemList.addAll(rnd.middleSquare(seed, number));
+        } else {
+            for (int i = 0; i < number; i++) {
+                itemList.add(rnd.randomClass());
+            }
+        }
+    }
+
+    private int getInitSeed() {
+        int seed = 0;
+        while (true) {
+            Long longSeed = System.currentTimeMillis();
+
+            String strSeed = longSeed.toString().substring(
+                    longSeed.toString().length() - 4,
+                    longSeed.toString().length());
+
+            seed = Integer.parseInt(strSeed);
+
+            if (seed < 1000) {
+                seed += 1000;
+            }
+
+            if (seed % 2 != 0) {
+                break;
+            }
+        }
+
+        return seed;
     }
 }
--- src/com/newvertex/randgen/util/Random.java
+++ src/com/newvertex/randgen/util/Random.java
@@ -1,5 +1,7 @@
 package com.newvertex.randgen.util;
 
+import java.util.ArrayList;
+
 public class Random {
 
     private int start;
@@ -16,5 +18,48 @@ public class Random {
 
     public int randomClass() {
         return new java.util.Random().nextInt((end - start) + start + 1);
+    }
+
+    public ArrayList<Integer> middleSquare(Integer seed, int number) {
+        ArrayList<Integer> result = null;
+
+        while (true) {
+            result = new ArrayList<>();
+            int i = 0;
+            while (i < number) {
+
+                Integer n = seed.toString().length();
+                Integer squre = (seed * seed);
+
+                String strSquare = squre.toString();
+
+                if (strSquare.length() == ((2 * n) - 1)) {
+                    strSquare = "0" + strSquare;
+                }
+
+                String substring = "";
+                try {
+                    if ((n % 2) == 0) {
+                        substring = strSquare.substring((n / 2), (strSquare.length() - (n / 2)));
+                        substring = "0." + substring;
+                        seed = Integer.parseInt(substring.substring(2));
+                        i++;
+                        result.add(seed % (end - start) + start + 1);
+                    } else {
+                        seed = Integer.parseInt(strSquare) + 1;
+                    }
+                } catch (Exception ex) {
+                    result.clear();
+                    seed += 1234;
+                }
+            }
+
+            if (result.size() >= number) {
+                break;
+            }
+            seed += 1234;
+        }
+
+        return result;
     }
 }
