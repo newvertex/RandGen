7d79019ed9a41e32265073c8dcfa6d9dc79d3bd6
Mojtaba Rabiei <m.rabiei.project@gmail.com>
2015-03-07 16:01:26 +0330

    Add randomClass method

--- src/com/newvertex/randgen/util/Random.java
+++ src/com/newvertex/randgen/util/Random.java
@@ -1,5 +1,20 @@
 package com.newvertex.randgen.util;
 
 public class Random {
-    
+
+    private int start;
+    private int end;
+
+    public Random() {
+        this(0, 1000);
+    }
+
+    public Random(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public int randomClass() {
+        return new java.util.Random().nextInt((end - start) + start + 1);
+    }
 }
